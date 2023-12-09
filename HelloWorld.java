07be0a4e1e509432663d5f39b615c69d260e3bfe
slgopi <153350554+slgopi@users.noreply.github.com>
2023-12-09 16:25:39 +0530

    Update HelloWorld.java
    commited substraction features

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,8 +4,10 @@ public class HelloWorld {
           int a =10;
           int b=20;
           int c = a+b;
+           int d= b-a;         
         System.out.println("Sum of two numbers = " + c);
-          System.out.println("This is feature edition changes");           
+          System.out.println("This is feature edition changes");  
+          System.out.println("substraction  of two numbers = " + d);
         }
 
 }
